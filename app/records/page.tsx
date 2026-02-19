bdcee58614d4668d93acbd6288ec9fbbfe08af3e
Enaga-OwO <rikku5910@outlook.com>
2026-02-19 13:24:13 +0900

    Delete app/records/page.tsx

--- app/records/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/records/page.tsx
-// /records へのアクセスをpublic/records.htmlにリダイレクト
-
-// import { redirect } from 'next/navigation';
-
-// export default function RecordsPage() {
-//   redirect('/records.html');
-// }
